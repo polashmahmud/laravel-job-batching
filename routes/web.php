ede5dc486f0b2fb0c51da92716824b934bea4323
Polash Mahmud <polashmahmud@gmail.com>
2024-12-16 22:20:47 +0600

    feat: Add ShowServer component and route for server details
    Add a new Livewire component 'ShowServer' to display server details.
    Create a new route '/server/{server}' to render the 'ShowServer' component.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Livewire\CreateServer;
+use App\Livewire\ShowServer;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
@@ -12,6 +13,10 @@ Route::view('dashboard', 'dashboard')
 Route::get('/server/create', CreateServer::class)
     ->middleware(['auth', 'verified'])
     ->name('server.create');
+
+Route::get('/server/{server}', ShowServer::class)
+    ->middleware(['auth', 'verified'])
+    ->name('server.show');
 
 Route::view('profile', 'profile')
     ->middleware(['auth'])
